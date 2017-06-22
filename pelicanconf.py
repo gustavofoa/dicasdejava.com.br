d92da1359b223e372bd8319a67aed495c6f741bb
Gustavo Furtado <gustavo.alves@mult-e.com.br>
2017-06-22 20:51:22 -0300

    Update pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,12 +5,15 @@ from __future__ import unicode_literals
 AUTHOR = 'Gustavo Furtado de Oliveira Alves'
 SITENAME = '{ Dicas de Java }'
 SITEURL = 'http://dicasdejava.com.br'
+CONTACT_EMAIL = 'gustavo@dicasdeprogramacao.com.br'
 
 PATH = 'content'
 
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = 'pt'
+DEFAULT_DATE_FORMAT = '%d de %B de %Y'
+
+DEFAULT_LANG = u'pt_BR'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,11 +22,9 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-DEFAULT_PAGINATION = 20
-
 RELATIVE_URLS = True
 
-STATIC_PATHS = ['extra/CNAME']
+STATIC_PATHS = ['extra/CNAME', 'images']
 
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'}
@@ -31,7 +32,7 @@ EXTRA_PATH_METADATA = {
 
 # Plugins
 PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['sitemap', 'post_stats', 'i18n_subsites']
+PLUGINS = ['sitemap']
 
 # Theme
 THEME = 'theme'
@@ -39,7 +40,48 @@ THEME = 'theme'
 BROWSER_COLOR = '#333'
 ROBOTS = 'index, follow'
 
+#Config slugs
 ARTICLE_URL = '{slug}/'
 ARTICLE_SAVE_AS = '{slug}/index.html'
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
+AUTHOR_URL = 'autor/{slug}/'
+AUTHOR_SAVE_AS = 'autor/{slug}/index.html'
+CATEGORY_URL = 'categoria/{slug}/'
+CATEGORY_SAVE_AS = 'categoria/{slug}/index.html'
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+ARCHIVES_URL = 'arquivo/'
+ARCHIVES_SAVE_AS = 'arquivo/index.html'
+
+# PAGINATED_DIRECT_TEMPLATES = ['archives']
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+DEFAULT_PAGINATION = 20
+SUMMARY_MAX_LENGTH = 40
+
+AUTHORS = {
+    'Gustavo Furtado de Oliveira Alves': {
+        'summary':'É mestre em computação aplicada pelo Institudo Nacional de Pesquisas Espaciais, '+
+                  'Engenheiro da Computação pela ETEP Faculdades e '+
+                  'Técnico em Informática pela Escola Técnica Pandiá Calógeras. '+
+                  'Possui as certificações SCJP, SCWCD e ASF e trabalha com desenvolvimento de softwares desde 2007.',
+        'image': '/images/author-gustavo.jpeg'
+    }
+}
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
