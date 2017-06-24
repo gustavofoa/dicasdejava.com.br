835cf60333a5d9c672991f9752f6efa00515f899
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-06-23 21:11:21 -0300

    Config Disqus as conditional

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "gustavofurtado"
+DISQUS_SITENAME = "dicas-de-java"
 GOOGLE_ANALYTICS = "UA-39997045-4"
