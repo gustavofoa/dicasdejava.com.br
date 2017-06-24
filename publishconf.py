62d52cab29173228c5ad99a3d32405921c4e9578
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-06-23 21:02:43 -0300

    Config Disqus

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "gustavofurtado"
 GOOGLE_ANALYTICS = "UA-39997045-4"
