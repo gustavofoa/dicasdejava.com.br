94db5bd197369c23d1c347d2610c471a05ebea7d
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-07-18 11:26:05 -0300

    Fix template issue.

--- publishconf.py
+++ publishconf.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-from pelicanconf import *
-import os
-import sys
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
+
+import os
+import sys
 sys.path.append(os.curdir)
+from pelicanconf import *
 
 SITEURL = 'http://dicasdejava.com.br'
 RELATIVE_URLS = False
