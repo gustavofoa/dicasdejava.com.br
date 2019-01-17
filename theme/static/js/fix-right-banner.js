65151e4a47a3695f1d463bab9ba3b63d3391ed2e
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2019-01-17 11:22:08 -0200

    fix banner over footer

--- theme/static/js/fix-right-banner.js
+++ theme/static/js/fix-right-banner.js
@@ -5,12 +5,12 @@ window.onscroll = function scrollFunction() {
     var last_adsense_banner = document.getElementById("sidebar-banner");
     var footer = document.querySelector('footer')
 
-    if (window.pageYOffset-100 > last_adsense_banner.offsetTop) {
+    if (window.pageYOffset-80 > last_adsense_banner.offsetTop) {
         if(adsense_position == 0)
             adsense_position = last_adsense_banner.offsetTop;
         last_adsense_banner.classList.add("fix-bannner");
     }
-    if (window.pageYOffset-100 < adsense_position || window.pageYOffset+600 > footer.offsetTop) {
+    if (window.pageYOffset-80 < adsense_position || window.pageYOffset+600 > footer.offsetTop) {
         adsense_position = 0;
         last_adsense_banner.classList.remove("fix-bannner");
     }
